78d8db84dc9c8da9e8c42b13fb14c054267bf710
sreenivas <sreenivasshenoy01@gmail.com>
2025-12-07 20:34:48 +0530

    Fix: Update the code files to remove unwanted characters from previous conflict

--- tourism_project/hosting/hosting.py
+++ tourism_project/hosting/hosting.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import os
 from huggingface_hub import HfApi, create_repo, upload_folder
 
@@ -25,31 +24,3 @@ upload_folder(
 )
 
 print(f"Deployment uploaded successfully: https://huggingface.co/spaces/{repo_id}")
-=======
-import os
-from huggingface_hub import HfApi, create_repo, upload_folder
-
-HF_USERNAME = "ShenoySreenivas"
-SPACE_NAME = "tourism-app"
-
-repo_id = f"{HF_USERNAME}/{SPACE_NAME}"
-
-api = HfApi(token=os.getenv("HF_TOKEN"))
-
-try:
-    api.repo_info(repo_id=repo_id, repo_type="space")
-    print(f"Space '{repo_id}' already exists.")
-except:
-    print(f"Creating new space {repo_id}")
-    create_repo(repo_id=repo_id, repo_type="space", private=False, space_sdk="docker")
-
-print("Uploading deployment files...")
-
-upload_folder(
-    folder_path="tourism_project/deployment",
-    repo_id=repo_id,
-    repo_type="space",
-)
-
-print(f"Deployment uploaded successfully: https://huggingface.co/spaces/{repo_id}")
->>>>>>> ab18067c2ed4da85e56bb22f8aee52f6970252a7
--- tourism_project/model_building/data_register.py
+++ tourism_project/model_building/data_register.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 from huggingface_hub import HfApi, create_repo
 import os
@@ -32,38 +31,3 @@ api.upload_folder(
 )
 
 print(f"Uploaded dataset files to Hugging Face: {repo_id}")
-=======
-from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
-from huggingface_hub import HfApi, create_repo
-import os
-
-# Set Hugging Face dataset repo info
-HF_USERNAME = "ShenoySreenivas"
-DATASET_NAME = "tourism-data"
-repo_id = f"{HF_USERNAME}/{DATASET_NAME}"
-repo_type = "dataset"
-
-# Path to local data folder
-local_data_folder = "tourism_project/data"
-
-# Hugging Face Token
-api = HfApi(token=os.getenv("HF_TOKEN"))
-
-# Check if dataset exists
-try:
-    api.repo_info(repo_id=repo_id, repo_type=repo_type)
-    print(f"Dataset '{repo_id}' already exists. Uploading files...")
-except RepositoryNotFoundError:
-    print(f"Dataset '{repo_id}' not found. Creating new dataset...")
-    create_repo(repo_id=repo_id, repo_type=repo_type, private=False)
-    print(f"Dataset '{repo_id}' created successfully.")
-
-# Upload CSV file(s)
-api.upload_folder(
-    folder_path=local_data_folder,
-    repo_id=repo_id,
-    repo_type=repo_type,
-)
-
-print(f"Uploaded dataset files to Hugging Face: {repo_id}")
->>>>>>> ab18067c2ed4da85e56bb22f8aee52f6970252a7
